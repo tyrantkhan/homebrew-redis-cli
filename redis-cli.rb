56cd6c2a2c5aa2f811ab8a296412d2a0ff082cac
ringohub <ringohub@gmail.com>
2016-03-18 23:21:32 +0900

    update

---
+++ redis-cli.rb
@@ -0,0 +1,15 @@
+class RedisCli < Formula
+  desc "Install the redis-cli only."
+  homepage "https://github.com/ringohub/homebrew-redis-cli"
+  version "3.0.7"
+  sha256 "b2a791c4ea3bb7268795c45c6321ea5abcc24457178373e6a6e3be6372737f23"
+  url "https://github.com/antirez/redis/archive/#{version}.tar.gz"
+
+  def install
+    system "make redis-cli"
+  end
+
+  test do
+    #system "false"
+  end
+end
