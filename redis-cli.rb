26ba114d981907dbb2fcd3c72dc829aebdd0b91b
Yoshiki Aoki <ringohub@gmail.com>
2017-08-30 19:45:50 +0900

    Update 4.0.1

--- redis-cli.rb
+++ redis-cli.rb
@@ -1,7 +1,7 @@
 class RedisCli < Formula
   desc "Install the redis-cli only."
   homepage "https://github.com/ringohub/homebrew-redis-cli"
-  version "3.0.7"
+  version "4.0.1"
   #sha256 "b2a791c4ea3bb7268795c45c6321ea5abcc24457178373e6a6e3be6372737f23"
   url "https://github.com/antirez/redis/archive/#{version}.tar.gz"
 
