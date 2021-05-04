ab2c0b40f6d1309d88c2b009d555c963046bbc85
Haris Khan <tyrantkhan@users.noreply.github.com>
2021-05-03 23:15:25 -0400

    Update redis-cli.rb

--- redis-cli.rb
+++ redis-cli.rb
@@ -2,7 +2,7 @@ class RedisCli < Formula
   desc "Install the redis-cli only."
   homepage "https://github.com/aoki/homebrew-redis-cli"
   version "6.0.5"
-  sha256 "e5aaf79ecea4cac64a838d4d2b597a5ecc19e50d2fcdf233295053fe12a9349d"
+  sha256 "f7ded6c27d48c20bc78e797046c79b6bc411121f0c2d7eead9fea50d6b0b6290"
   url "https://github.com/antirez/redis/archive/#{version}.tar.gz"
 
   def install
